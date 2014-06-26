326b18604f80313d2c72f3b5d82e4599084d2e96
Anh Tuan Nguyen <contactme@anhtuann.com>
2014-06-26 08:57:54 +0700

    parseWeather works

--- p01-fetch_current_weather/weather.py
+++ p01-fetch_current_weather/weather.py
@@ -1,9 +1,9 @@
 import urllib
 import re
+import json
 
-ww_apikey = "e6242d7fb7400876f8f047e897b4f5a2f61c7246"
-ww_weather_url = "http://api.worldweatheronline.com/free/v1/weather.ashx"
-ww_city_url = "http://api.worldweatheronline.com/free/v1/search.ashx"
+wwo_apikey = "e6242d7fb7400876f8f047e897b4f5a2f61c7246"
+wwo_weather_url = "http://api.worldweatheronline.com/free/v1/weather.ashx"
 
 def findIP():
   url = "http://checkip.dyndns.org"
@@ -15,9 +15,14 @@ def findIP():
 myIP = findIP()
 
 def queryWWO(api_url,ip):
-  url = api_url + "?" + "key=" + ww_apikey + "&q=" + ip
+  url = api_url + "?" + "key=" + wwo_apikey + "&q=" + ip + "&includeLocation=yes" + "&format=json"
   query = urllib.urlopen(url).read()
   return query
 
-print(queryWWO(ww_city_url,myIP))
-print(queryWWO(ww_weather_url,myIP))
+def parseWeather(data_json):
+  weather = json.loads(data_json)
+  weather_conditions = weather['data']['current_condition'][0].items()
+  for item in weather_conditions:
+    print('{key} : {value}'.format(key=item[0], value=item[1]))
+
+parseWeather(queryWWO(wwo_weather_url,myIP))
