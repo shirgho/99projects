379e7cf6568035ab6d811b13e1bf17037085aae2
Anh Tuan Nguyen <contactme@anhtuann.com>
2014-07-07 11:24:31 +0700

    Error handling and requests library added

--- p03_page_scraper/scraper.py
+++ p03_page_scraper/scraper.py
@@ -1,11 +1,11 @@
-import urllib2
+import requests
 import re
 import json
 
 base_url = 'http://9gag.com/trending'
 
 def connectSite(url):
-    page = urllib2.urlopen(url).read()
+    page = requests.get(url).text
     return page
 
 def getLinks(page):
@@ -24,13 +24,30 @@ def exploreLinks(linkList):
     indexArticle = 1
     indexLinks = dict() 
     for link in linkList:
-        page = connectSite(link)
+        page = connectSite(link.encode('utf-8'))
         indexLinks[indexArticle] = parseLinks(page)
+        print indexLinks[indexArticle]
         indexArticle += 1
     return indexLinks
 
-if __name__ == '__main__':
-    links = getLinks(connectSite(base_url))
-    articles = exploreLinks(links)
-    with open('indexedArticles.txt', 'w') as articlesJson:
+def getData(url, max_tries = 5):
+    tries = 0
+    articles = None
+    while articles is None and tries < max_tries:
+        try:
+            data = connectSite(base_url)
+            links = getLinks(data)
+            articles = exploreLinks(links)
+        except Exception as error:
+            print error
+            tries += 1
+    return articles
+
+def genJson(articles):
+    with open('indexedArticles.json', 'w') as articlesJson:
         json.dump(articles, articlesJson)
+    return 'Articles indexed'
+
+if __name__ == '__main__':
+    articles = getData(base_url)
+    genJson(articles)
