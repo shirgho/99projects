c9e46a801819dd8a8f0c65c771942720a710dea7
Anh Tuan Nguyen <contactme@anhtuann.com>
2014-07-16 17:07:09 +0700

    end of P04, some improvements left

--- p04_sudoku_generator/sudoku.py
+++ p04_sudoku_generator/sudoku.py
@@ -1,6 +1,7 @@
 import random
 
 def gridSudoku(sudoku_values):
+#creates a grid for displaying sudoku
     sudoku_ascii = str()
     for idx_row, row in enumerate(sudoku_values):
         row_ascii = ' '
@@ -20,6 +21,7 @@ def gridSudoku(sudoku_values):
     return sudoku_ascii
 
 def genEmptySudoku():
+#generates a sudoku with only zeros in it
     empty_sudoku = list()
     for row in xrange(9):
         row_list = []
@@ -28,27 +30,19 @@ def genEmptySudoku():
         empty_sudoku.append(row_list)        
     return empty_sudoku
 
-def randomSudoku():
-    sudoku = list()
-    for row in xrange(9):
-        row_list = list()
-        for number in xrange(9):
-            row_list.append(random.randint(1,9))
-        sudoku.append(row_list)
-    return sudoku
-            
-    return [[random.randint(0,9)]*9]*9
-
 def checkRow(number, row, sudoku):
+#checks if a number is NOT in a line
     return sudoku[row].count(number) == 0
 
 def checkColumn(number, column, sudoku):
+#checks if a number is NOT in a column
     column_list = list()
     for row in sudoku:
         column_list.append(row[column])
     return column_list.count(number) == 0
 
 def checkBlock(number, block, sudoku):
+#checks if a number is NOT in a block
     block_list = list()
     block_starters = [(0,0),(0,3),(0,6),(3,0),(3,3),(3,6),(6,0),(6,3),(6,6)]
     row_start, column_start  = block_starters[block]
@@ -58,6 +52,7 @@ def checkBlock(number, block, sudoku):
     return block_list.count(number) == 0
 
 def findBlock(coordinates_number):
+#finds the block of a number given its coordinates
     row, column = coordinates_number
     if row < 3:
         if column < 3:
@@ -82,11 +77,13 @@ def findBlock(coordinates_number):
             return 8
     
 def checkRules(number, coordinates_number, sudoku):
+#checks if a number doesn't violate sudoku rules
     row, column = coordinates_number
     block = findBlock(coordinates_number)
     return checkRow(number, row, sudoku) and checkColumn(number, column, sudoku) and checkBlock(number, block, sudoku)
 
 def genNumber(coordinates_number, sudoku):
+#generates a number according to sudoku rules
     row_number, column_number = coordinates_number
     possibilities = list()
     for number in xrange(1,10):
@@ -95,24 +92,42 @@ def genNumber(coordinates_number, sudoku):
     number = random.choice(possibilities)
     return number
     
-def genSudoku():
+def genSudokuSolution():
+#tries to create a full sudoku
     sudoku_values = genEmptySudoku()
     for row in xrange(9):
         for column in xrange(9):
             sudoku_values[row][column] = genNumber((row,column),sudoku_values)
     return sudoku_values
 
-def attemptGenSudoku(trials = 1000):
+def attemptGenSudokuSolution(trials = 1000):
+#tries to create a full sudoku for the number of trials
     sudoku = list()
     for trial in xrange(trials):
         try:
-            sudoku = genSudoku()
+            sudoku = genSudokuSolution()
             print "Success after {} attempts".format(trial+1)
             return sudoku
         except IndexError:
             pass
     return genEmptySudoku()
     
-    
+def genSudokuProblem(sudoku_solution):
+#removes numbers from a full sudoku to create a sudoku problem
+    sudoku_problem = list(sudoku_solution)
+    possibilities = range(81)
+    while len(possibilities) > 18:
+        index = random.choice(possibilities)
+        possibilities.remove(index)
+        row = index // 9
+        column = index % 9
+        sudoku_problem[row][column] = 0
+    return sudoku_problem
+
 if __name__ == '__main__':
-    print gridSudoku(attemptGenSudoku())
+    sudoku_sol = attemptGenSudokuSolution()
+    print gridSudoku(sudoku_sol)
+
+    sudoku_pb = genSudokuProblem(sudoku_sol)
+    print gridSudoku(sudoku_pb)
+
